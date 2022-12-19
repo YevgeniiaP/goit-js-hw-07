bb725f0af3513a86f2d9440710d25f6f6b92c31f
Yevgeniia Pererva <jane.iljasowa@gmail.com>
2022-12-19 04:03:56 +0200

    add solutions to ex 1,2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,43 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryCards  = createGalleryItems(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', galleryCards );
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+            </div>`;
+            })
+            .join("");
+};
+
+function imageOpen(event) {
+    event.preventDefault();
+    
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+        
+
+    const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">`)
+    
+    instance.show()
+    
+};
+
+
+galleryList.addEventListener('click', imageOpen);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+
+const galleryCards = createGalleryItems(galleryItems);
+galleryList.insertAdjacentHTML('beforeend', galleryCards );
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+            </a>
+            </div>`;
+            })
+            .join("");
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { 'captionsData': 'alt', 'captionPosition': "bottom",'captionDelay': '250'});
